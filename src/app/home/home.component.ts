1bb99042e78236dbc9a96f5a4d88cc0ecb4b87e4
Ivan <redattack148@gmail.com>
2022-07-03 23:27:00 +0300

    angola

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {Router} from "@angular/router"
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  result:any=[]
+  constructor(private http:HttpClient,private router:Router) {
+  }
+  public goToPlanet(id:any){
+    this.router.navigate(['planets',id])
+  }
+
+    ngOnInit(): void {
+
+      for(let i=1;i<=6;i++)
+      {
+        this.http.get(`https://swapi.dev/api/planets/?page=${i}`)
+        .subscribe((response)=>{
+          this.result.push(response);
+          console.log(this.result)
+        })
+      }
+      
+     
+    }
+  }
